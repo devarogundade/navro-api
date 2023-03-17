121e62317b370e26133c4942e780107a8a5dd23b
devarogundade <devarogundade@gmail.com>
2023-03-17 17:10:45 +0100

    update product model

--- app/models/products.model.js
+++ app/models/products.model.js
@@ -1,8 +1,36 @@
 
 module.exports = mongoose => {
     const schema = mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        images: {
+            type: Array
+        },
+        category: {
+            type: Array,
+            required: true
+        },
+        colors: {
+            type: Array,
+            required: true
+        },
+        size: {
+            type: String
+        },
         price: {
             type: Number
+        },
+        brand: {
+            type: String
+        },
+        url: {
+            type: String,
+            required: true
         }
     }, { timestamps: false });
 
