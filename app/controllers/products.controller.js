e176ccd431f1d7e59fad4463dfa31e9f76447ea1
devarogundade <devarogundade@gmail.com>
2023-03-17 17:50:58 +0100

    update controller func

--- app/controllers/products.controller.js
+++ app/controllers/products.controller.js
@@ -4,7 +4,16 @@ const Product = db.product;
 
 // Retrieve all Product from the database.
 exports.findAll = (req, res) => {
-    Product.find(req.query)
+
+    const colors = req.query.colors ? req.query.colors.split(',') : null
+    const categories = req.query.categories ? req.query.categories.split(',') : null
+    
+    const query = {}
+
+    if (colors) query.colors = { $in: colors }
+    if (categories) query.categories = { $in: categories }
+
+    Product.find(query)
         .then(data => {
             res.send(data);
         })
